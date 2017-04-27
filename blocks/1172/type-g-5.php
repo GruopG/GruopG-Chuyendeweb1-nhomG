25898baa806085a8450b9f9e09a236cd7b489735
GruopG <GruopG@users.noreply.github.com>
2017-04-28 05:17:19 +0700

    Add files via upload

---
+++ blocks/1172/type-g-5.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<!--Ket Noi File Less -->
+	<?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('./less/type-g-5.less', 'css/type-g-5.css');
+    ?>
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/type-g-5.css">
+</head>
+
+<body>
+	<div class="type-g-5">
+		<section class="new-deal">
+			<div class="container">
+		   		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal deal-block">
+					<div class="item-slide">
+						<div class="box-img">
+							<img src="images/swallowed_by_nature_by_danutza88.jpg" alt="dasdas"/>
+							<div class="text-wrap">
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">								
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+						<div class="slide-hover">
+							<div class="text-wrap">
+								<p>MyFlightSearch gets you flight tickets at the best price. MyFlightSearch gets you flight tickets at the best price.</p>
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal">
+					<div class="item-slide">
+						<div class="box-img">
+							<img src="images/swallowed_by_nature_by_danutza88.jpg" alt="dasdas"/>
+							<div class="text-wrap">
+							<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">								
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+						<div class="slide-hover">
+							<div class="text-wrap">
+								<p>MyFlightSearch gets you flight tickets at the best price. MyFlightSearch gets you flight tickets at the best price.</p>
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal">
+					<div class="item-slide">
+						<div class="box-img">
+							<img src="images/swallowed_by_nature_by_danutza88.jpg" alt="dasdas"/>
+							<div class="text-wrap">
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">								
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+						<div class="slide-hover">
+							<div class="text-wrap">
+								<p>MyFlightSearch gets you flight tickets at the best price. MyFlightSearch gets you flight tickets at the best price.</p>
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal">
+					<div class="item-slide">
+						<div class="box-img">
+							<img src="images/swallowed_by_nature_by_danutza88.jpg" alt="dasdas"/>
+							<div class="text-wrap">
+							<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">								
+								<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+						<div class="slide-hover">
+							<div class="text-wrap">
+								<p>MyFlightSearch gets you flight tickets at the best price. MyFlightSearch gets you flight tickets at the best price.</p>
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal">
+					<div class="item-slide">
+						<div class="box-img">
+							<img src="images/swallowed_by_nature_by_danutza88.jpg" alt="dasdas"/>
+							<div class="text-wrap">
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">								
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+						<div class="slide-hover">
+							<div class="text-wrap">
+								<p>MyFlightSearch gets you flight tickets at the best price. MyFlightSearch gets you flight tickets at the best price.</p>
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal">
+					<div class="item-slide">
+						<div class="box-img">
+							<img src="images/swallowed_by_nature_by_danutza88.jpg" alt="dasdas"/>
+							<div class="text-wrap">
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">								
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+						<div class="slide-hover">
+							<div class="text-wrap">
+								<p>MyFlightSearch gets you flight tickets at the best price. MyFlightSearch gets you flight tickets at the best price.</p>
+								<h4>Paris europe  <span class="deal-data"><span class="fa fa-calendar"></span> Sun, Jan 29 - Tue, Jan 31</span></h4>
+								<div class="desc">									
+									<span>Starting from</span>
+									<h3>$1000</h3>
+								</div>
+								<div class="book-now-c">
+									<a href="#">Book Now</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+		 	</div>
+		</section>
+	</div>
+
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
